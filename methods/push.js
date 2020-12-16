415f48658dfae7d0a2b5e04e1e2ffe079f74a299
mbdeme <mbdeme@univ-paris1.fr>
2020-12-16 12:13:18 +0100

    modif title Esup OTP Push to Esup Auth

--- methods/push.js
+++ methods/push.js
@@ -24,7 +24,7 @@ exports.send_message = function (user, req, res, next) {
     var message = new gcm.Message({
         priority: "high",
         data: {
-            title: "Esup-OTP-Push",
+            title: "Esup Auth",
             body: "Demande de connexion à votre compte",
             text: "Demande de connexion à votre compte",
             action: 'auth',
@@ -244,7 +244,7 @@ function alert_deactivate(user) {
     var message = new gcm.Message({
         priority: "high",
         data: {
-            title: "Esup-OTP-Push",
+            title: "Esup Auth",
             body: "Les notifications push ont été désactivées pour votre compte",
             text: "Les notifications push ont été désactivées pour votre compte",
             action: "desync"
